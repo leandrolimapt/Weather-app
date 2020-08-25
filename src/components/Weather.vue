6fd05feb34b7e7e0987a90340254ace83a93f682
Leandro <business.leandrolima@gmail.com>
2020-08-25 18:47:16 +0100

    Basic interface with welcome page, error page & result page

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,43 +1,53 @@
 <template>
-  <main>
-    <div class="search-box">
-      <input 
-      type="text" 
-      class="search-bar" 
-      placeholder="Search..."
-      v-model="query"
-      @keyup="fetchWeather"
-      />
-    </div>
-    <div class="weather-wrap" v-if="weather.main">
+  <div class="search-box">
+    <input 
+    type="text" 
+    class="search-bar" 
+    placeholder="Search here ..."
+    v-model="query"
+    @keyup="fetchWeather"
+    />
+  </div>
+  <div class="weather-wrap" v-if="weather.main">
+    <div class="content">
+      <p class="title">Weather Forecast</p>
       <div class="location-box">
         <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
         <div class="date">{{ dateBuilder() }}</div>
       </div>
-      <div class="weather-box">
-        <div id="image" :class="weather.main && 
-          weather.weather[0].main.toLowerCase ()"> 
+    </div>
+    
+    <div class="weather-box">
+      <div id="image" :class="weather.main && 
+        weather.weather[0].main.toLowerCase ()"> 
+      </div>
+      <div class="temp">{{Math.round(weather.main.temp)}}<label class="degree"></label></div>
+      <div class="weather"><span id="weather">{{weather.weather[0].main}}</span></div>   
+      <div class="more-info">
+        <div class="item">{{weather.wind.speed}} km/h
+          <div class="description">Wind</div>
         </div>
-        <div class="temp">{{Math.round(weather.main.temp)}}<label class="degree">°C</label></div>
-        <div class="weather"><span id="weather">{{weather.weather[0].main}}</span></div>   
-        <div class="more-info">
-          <div class="item">{{weather.wind.speed}} km/h
-            <div class="description">Wind</div>
-          </div>
-          <div class="item">{{weather.main.humidity}}%
-            <div class="description">Humidity</div>
-          </div>
-          <div class="item">{{Math.round(weather.main.temp_max)}}°
-            <div class="description">Maximum</div>
-          </div>
+        <div class="item">{{weather.main.humidity}}%
+          <div class="description">Humidity</div>
+        </div>
+        <div class="item">{{Math.round(weather.main.temp_max)}}°
+          <div class="description">Maximum</div>
         </div>
       </div>
-    </div> 
-    <div>
-      <h1>{{error}}</h1>
     </div>
-
-  </main>
+  </div> 
+  <div class="error" v-else-if="error != null">
+    <img src="../assets/img/cry.svg">
+    <h1>{{error}}</h1>
+  </div>
+  <div class="landing-page" v-else>
+    <div class="container">
+      <div class="content">
+        <h4>Weather APP</h4>
+        <h1>Welcome</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -57,7 +67,7 @@ export default {
       if (e.key == "Enter"){
         let request = await fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`) 
         if (request.status == 404) {
-          this.error = 'Não encontrado';
+          this.error = 'City not found';
         } else {
           this.error = null;  
         }
